ccc0151810ecfc7659b2ad07483e7cc0f3e58225
QuinteroVA <angelqvargas@yahoo,com>
2024-02-07 23:47:37 -0500

    Creación de vistas y plantillas

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Estudiante, Profesor, Curso
 
-# Create your views here.
+def lista_estudiantes(request):
+  estudiantes = Estudiante.objects.all()
+  return render(request, 'gestion/lista_estudiantes.html', {'estudiantes': estudiantes})
+
+def lista_profesores(request):
+  profesores = Profesor.objects.all()
+  return render(request, 'gestion/lista_profesores.html', {'profesores': profesores})
+
+def lista_cursos(request):
+  cursos = Curso.objects.all()
+  return render(request, 'gestion/lista_cursos.html', {'cursos': cursos})
